617a765e2e1b9f7c4bdd2ac2a0c8e30b1b7a5f14
trammellmaurice <zacharymtrammell@gmail.com>
2021-02-06 12:53:54 -0500

    cleanup and movements added

---
+++ manual_mov/up.py
@@ -0,0 +1,23 @@
+import Jetson.GPIO as GPIO 
+import time
+
+# pin definitions
+GPIO.setmode(GPIO.BOARD)
+
+#7 = down
+#12 = up
+# 13 = fire
+# setup channel
+channels = [7,12,13,19,21]
+
+DOWN, UP, FIRE,RIGHT,LEFT = 7,12,13,19,21
+GPIO.setup(channels,GPIO.OUT,initial=GPIO.LOW)  
+
+
+GPIO.output(UP,GPIO.HIGH)
+
+time.sleep(0.1)
+
+GPIO.output(UP,GPIO.LOW)
+
+GPIO.cleanup()
