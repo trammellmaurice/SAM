f4adb165fa4d402446b7fda5385957d0e30eb70e
mars <zacharymtrammell@mail.com>
2021-02-07 18:08:41 -0500

    bugs

--- manual_mov/teleop.py
+++ manual_mov/teleop.py
@@ -29,42 +29,40 @@ for i in range(0,10):
 while video.isOpened():
     GPIO.output(ALL,GPIO.LOW)
     time.sleep(0.01)
-    try:
-        events = get_gamepad()
-        for event in events:
-            if event.code == "BTN_BASE3" and event.state == 1:
-                SAM = False
-                print("MANUAL CONTROL ENGAGE")
-            elif event.code == "BTN_BASE" and event.state == 0:
-                SAM = True
-                print("SAM CONTROL ENGAGE")
-            if not SAM:
-                if event.code == "BTN_DEAD" and event.state == 1:
-                    if not ARMED:
-                        ARMED = True
-                        print("ARMED")
-                    else:
-                        ARMED = False
-                        print("DISARMED")
-                elif event.code == "BTN_PINKIE" and event.state == 1:
-                    GPIO.output(RIGHT,GPIO.HIGH)
-                    print("RIGHT")
-                elif event.code == "BTN_BASE2" and event.state == 1:
-                    GPIO.output(LEFT,GPIO.HIGH)
-                    print("LEFT")
-                elif event.code == "BTN_TOP2" and event.state == 1:
-                    GPIO.output(UP,GPIO.HIGH)
-                    print("UP")
-                elif event.code == "BTN_BASE" and event.state == 1:
-                    GPIO.output(DOWN,GPIO.HIGH)
-                    print("DOWN")
 
-                elif event.code == "BTN_BASE6" and event.state == 1:
-                    if ARMED:
-                        GPIO.output(FIRE,GPIO.HIGH)
-                        print("FIRE")
-                    else:
-                        print("MUST ARM")
-                time.sleep(0.01)
-    except:
-        pass
+    events = get_gamepad()
+    for event in events:
+        if event.code == "BTN_BASE3" and event.state == 1:
+            SAM = False
+            print("MANUAL CONTROL ENGAGE")
+        elif event.code == "BTN_BASE" and event.state == 0:
+            SAM = True
+            print("SAM CONTROL ENGAGE")
+        if not SAM:
+            if event.code == "BTN_DEAD" and event.state == 1:
+                if not ARMED:
+                    ARMED = True
+                    print("ARMED")
+                else:
+                    ARMED = False
+                    print("DISARMED")
+            elif event.code == "BTN_PINKIE" and event.state == 1:
+                GPIO.output(RIGHT,GPIO.HIGH)
+                print("RIGHT")
+            elif event.code == "BTN_BASE2" and event.state == 1:
+                GPIO.output(LEFT,GPIO.HIGH)
+                print("LEFT")
+            elif event.code == "BTN_TOP2" and event.state == 1:
+                GPIO.output(UP,GPIO.HIGH)
+                print("UP")
+            elif event.code == "BTN_BASE" and event.state == 1:
+                GPIO.output(DOWN,GPIO.HIGH)
+                print("DOWN")
+
+            elif event.code == "BTN_BASE6" and event.state == 1:
+                if ARMED:
+                    GPIO.output(FIRE,GPIO.HIGH)
+                    print("FIRE")
+                else:
+                    print("MUST ARM")
+            time.sleep(0.01)
