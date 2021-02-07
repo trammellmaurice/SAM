49cf96bd475a1b420498922e94e1885142a86a33
mars <zacharymtrammell@mail.com>
2021-02-07 16:49:05 -0500

    test

--- manual_mov/teleop.py
+++ manual_mov/teleop.py
@@ -26,42 +26,42 @@ for i in range(0,10):
      ok, frame = video.read()
 
 while video.isOpened():
-    GPIO.output(ALL,GPIO.LOW)
-    time.sleep(0.01)
+    # GPIO.output(ALL,GPIO.LOW)
+    # time.sleep(0.01)
     events = get_gamepad()
     for event in events:
-        # if event.ev_type != "Sync" and event.ev_type != "Absolute":
-        #     print(event.ev_type, event.code, event.state)
-        if event.code == "BTN_TL2" and event.state == 1:
-            SAM = False
-            print("MANUAL CONTROL ENGAGE")
-        elif event.code == "BTN_TL2" and event.state == 0:
-            SAM = True
-            print("SAM CONTROL ENGAGE")
-        if not SAM:
-            if event.code == "BTN_WEST" and event.state == 1:
-                ARMED = True
-                print("ARMED")
-            elif event.code == "BTN_NORTH" and event.state == 1:
-                ARMED = False
-                print("DISARMED")
-            elif event.code == "BTN_DPAD_RIGHT" and event.state == 1:
-                GPIO.output(RIGHT,GPIO.HIGH)
-                print("RIGHT")
-            elif event.code == "BTN_DPAD_LEFT" and event.state == 1:
-                GPIO.output(LEFT,GPIO.HIGH)
-                print("LEFT")
-            elif event.code == "BTN_DPAD_UP" and event.state == 1:
-                GPIO.output(UP,GPIO.HIGH)
-                print("UP")
-            elif event.code == "BTN_DPAD_DOWN" and event.state == 1:
-                GPIO.output(DOWN,GPIO.HIGH)
-                print("DOWN")
-
-            elif event.code == "BTN_TR" and event.state == 1:
-                if ARMED:
-                    GPIO.output(FIRE,GPIO.HIGH)
-                    print("FIRE")
-                else:
-                    print("MUST ARM")
-            time.sleep(0.01)
+        if event.ev_type != "Sync" and event.ev_type != "Absolute":
+            print(event.ev_type, event.code, event.state)
+        # if event.code == "BTN_TL2" and event.state == 1:
+        #     SAM = False
+        #     print("MANUAL CONTROL ENGAGE")
+        # elif event.code == "BTN_TL2" and event.state == 0:
+        #     SAM = True
+        #     print("SAM CONTROL ENGAGE")
+        # if not SAM:
+        #     if event.code == "BTN_WEST" and event.state == 1:
+        #         ARMED = True
+        #         print("ARMED")
+        #     elif event.code == "BTN_NORTH" and event.state == 1:
+        #         ARMED = False
+        #         print("DISARMED")
+        #     elif event.code == "BTN_DPAD_RIGHT" and event.state == 1:
+        #         GPIO.output(RIGHT,GPIO.HIGH)
+        #         print("RIGHT")
+        #     elif event.code == "BTN_DPAD_LEFT" and event.state == 1:
+        #         GPIO.output(LEFT,GPIO.HIGH)
+        #         print("LEFT")
+        #     elif event.code == "BTN_DPAD_UP" and event.state == 1:
+        #         GPIO.output(UP,GPIO.HIGH)
+        #         print("UP")
+        #     elif event.code == "BTN_DPAD_DOWN" and event.state == 1:
+        #         GPIO.output(DOWN,GPIO.HIGH)
+        #         print("DOWN")
+        #
+        #     elif event.code == "BTN_TR" and event.state == 1:
+        #         if ARMED:
+        #             GPIO.output(FIRE,GPIO.HIGH)
+        #             print("FIRE")
+        #         else:
+        #             print("MUST ARM")
+        #     time.sleep(0.01)
