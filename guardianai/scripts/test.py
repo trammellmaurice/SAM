7d6089d9b616b9d25f9137a9e495536f02fbf3ab
marsz <zacharymtrammell@mail.com>
2021-03-07 17:11:18 -0500

    Try video feed test.py

--- guardianai/scripts/test.py
+++ guardianai/scripts/test.py
@@ -11,8 +11,8 @@ import cv2
 import numpy as np
 
 # ros libraries
-import rospy
-from std_msgs.msg import String
+# import rospy
+# from std_msgs.msg import String
 
 # inference libraries
 # import jetson.inference
@@ -21,4 +21,11 @@ from std_msgs.msg import String
 # # set up camera (use open cv)
 video = cv2.VideoCapture(0)
 
+while video.isOpened():
+    # DISPLAY FRAME
+    cv2.imshow('TURRET',frame)
+    # quit on ESC
+    if cv2.waitKey(1) & 0xFF == 27:
+        sys.exit()
+        
 # print(cv2.__version__)
