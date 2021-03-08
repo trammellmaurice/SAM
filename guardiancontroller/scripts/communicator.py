4dd43bc42092d99a093f00e002b593a3bb084e9f
Mars Trammell <zacharymtrammell@gmail.com>
2021-03-08 15:16:35 -0500

    communicator music update

--- guardiancontroller/scripts/communicator.py
+++ guardiancontroller/scripts/communicator.py
@@ -2,6 +2,7 @@
 
 import time
 import subprocess
+import threading 
 
 import rospy
 from std_msgs.msg import String
@@ -13,18 +14,8 @@ STATES = {'help': ['mpg321', '-l 0','/media/pi/BE82-0E13/sad.mp3'],
 last = None
 process = None
 
-def callback(state):
-    nodes = subprocess.check_output(['rosnode','list'])
-    running = str(nodes).find('guardian_ai')
-    if running == -1 and last != 'h':
-        state.data = 'h'
-        if (last == 'a' or last == 'i'):
-            global process
-            process.kill()
-        global STATES
-        global process
-        process = subprocess.Popen(STATES['help'])
 
+def callback(state):
     if state.data == "a" and last != 'a':
         if (last == 'h' or last == 'i'):
             global process
@@ -42,9 +33,18 @@ def callback(state):
         global process
         process = subprocess.Popen(STATES['idle'])
 
+    elif state.data == "h" and last != 'h':
+        if (last == 'i' or last == 'a'):
+            global process
+            process.kill()
+        global STATES
+        global process
+        process = subprocess.Popen(STATES['help'])
+
     global last
     last = state.data
 
 rospy.init_node('communicator',anonymous=True)
+threading.Thread(target=running).start()
 rospy.Subscriber('status',String,callback)
 rospy.spin()
