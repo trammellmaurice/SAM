a6f4f8c9f341c34335f41403fbb070aac5882514
Cmeacham <65180790+Cmeacham@users.noreply.github.com>
2021-03-11 21:23:09 -0500

    Signal Sender file

--- SIGINT/SignalCommand.py
+++ SIGINT/SignalCommand.py
@@ -10,15 +10,26 @@
 # -----------------------------------------------------------------------------
 
 # Name: Christopher Meacham
-# Subject: Capstone Signal Intelligence Part 1
-# Description: This code uses the os library to run terminal commands through python code. The reason for this is to make it easier to tell the rcv which basic direction the user wants to go. The command library so far is sigstop, back, forward, left, and right. This can all be found in the function Commands() which utilizes the HackRF tool called hackrf_transfer. This tool send a prerecorded signal for each direction. The other arguments of the tool are the frequency and sample rate. There is another command just in case the HackRF is unable to send a signal. The command to error check this is reset. The command reset calls on the function Sync(), which is in charge of sending a sync signal to the rcv as well as troubleshooting the HackRF "unable to send signal" error. It resets the HackRF through the use of the HackRF tool known as hackrf_spiflash -R. 
+# Subject: Capstone Signal Intelligence
+''' 
+Description: This code uses the os library to run terminal commands through python code. The reason for this is to make it easier to tell the rcv which basic direction the user 
+wants to go. The command library so far is sigstop, back, forward, left, and right. This can all be found in the function Commands() which utilizes the HackRF tool called 
+hackrf_transfer. This tool send a prerecorded signal for each direction. The other arguments of the tool are the frequency and sample rate. There is another command just in
+case the HackRF is unable to send a signal. The command to error check this is reset. The command reset calls on the function Sync(), which is in charge of sending a sync 
+signal to the rcv as well as troubleshooting the HackRF "unable to send signal" error. It resets the HackRF through the use of the HackRF tool known as hackrf_spiflash -R. 
 
-# *Update* The code now recieves commands from CommandInput.py. The functionality of that file can be found in its description. Om this side, the code reads from CommandRequests.txt for any commands. If there's a command, then it will read it off the file and execute the signal needed to be sent to the SDR. Once it has completed the command, it clears the text file so that more commands can be sent over. 
+*Update* The code now recieves commands from CommandInput.py. The functionality of that file can be found in its description. Om this side, the code reads from 
+CommandRequests.txt for any commands. If there's a command, then it will read it off the file and execute the signal needed to be sent to the SDR. Once it has completed the
+command, it clears the text file so that more commands can be sent over. 
 
-# *Update 05MAR21* The code now can receive commands from any python file that uses sockets and connects to this one via those socket capabilities. I've gotten rid of the text file that was between the two files so that it is more streamline and symbiotic with other python files. Also, the text file solution was not the best solution since it required extra unnecessary steps that needed to be taken in order to know when a command has been sent through.
+*Update 05MAR21* The code now can receive commands from any python file that uses sockets and connects to this one via those socket capabilities. I've gotten rid of the 
+text file that was between the two files so that it is more streamline and symbiotic with other python files. Also, the text file solution was not the best solution since 
+it required extra unnecessary steps that needed to be taken in order to know when a command has been sent through.
 
-# *Update 11MAR21* Hackrf_transfer was no longer working so I had to figure out a alternative. That alternative was through the use of GNUradio's python transcribing abilities. The python files that have taken the place of the hackrf_transfer commands are the modified version of those transcriptions. I say "modified" because I had to take out the GUI so that all it does is send the signal for a certain amount of time and then return to the prompt. Just note that the hackrf_spiflash does still work and it may be needed.
-
+*Update 11MAR21* Hackrf_transfer was no longer working so I had to figure out a alternative. That alternative was through the use of GNUradio's python transcribing abilities.
+The python files that have taken the place of the hackrf_transfer commands are the modified version of those transcriptions. I say "modified" because I had to take out the GUI
+so that all it does is send the signal for a certain amount of time and then return to the prompt. Just note that the hackrf_spiflash does still work and it may be needed.
+'''
 
 import os, time
 import socket
@@ -62,7 +73,7 @@ def Commands(ComDir):
         
         elif Comm == "right":
             print("Right")
-            os.system("python2 RightGNU.py")
+            os.system("python2 RightGNU.py") # File not created yet
             
         elif Comm == "left":
             print('Left')
@@ -70,10 +81,10 @@ def Commands(ComDir):
         
         elif Comm == "forward":
             print("Forward")
-            os.system("python2 ForwardGNU.py")
+            os.system("python2 ForwardGNU.py") # File not created yet
         
         elif Comm == "back":
-            os.system("python2 BackGNU.py")
+            os.system("python2 BackGNU.py") # File not created yet
             
         else: 
             print("\nPlease use the commands: Forward, Back, Left, Right, or Reset. Try again.\nThis was your command: "+Comm)
